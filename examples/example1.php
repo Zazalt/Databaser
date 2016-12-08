a159cc9dafdc2090115fde80b638835fc68e51dc
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-12-08 23:24:01 +0200

    The cake is a lie.

--- examples/example1.php
+++ examples/example1.php
@@ -3,11 +3,13 @@
 /**
  * Will return an array with all tables/entities and rows
  */
-$entities = Zazalt\Databaser\Databaser::getInstance()
-                ->setEngine(\Zazalt\Omen\Omen::ENGINE_POSTGRESQL)
-                //->setHost('127.0.0.1') // If not set, default is 127.0.0.1
-                //->setPort()            // If not set, default is 5432 (PostgreSQL default port)
-                ->setUsername('your_username')
-                ->setPassword('your_password')
-                ->setDatabase('your_database_name')
-                ->run();
+print_r(
+    Zazalt\Databaser\Databaser::getInstance()
+        ->setEngine(\Zazalt\Omen\Omen::ENGINE_POSTGRESQL)
+        //->setHost('127.0.0.1') // If not set, default is 127.0.0.1
+        //->setPort()            // If not set, default is 5432 (PostgreSQL default port)
+        ->setUsername('your_username')
+        ->setPassword('your_password')
+        ->setDatabase('your_database_name')
+        ->run()
+);
